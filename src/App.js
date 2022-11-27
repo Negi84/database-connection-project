3e7bd9eaa92a41ec483c765ac7fdcdf2ffc48286
Vipin Negi <vipin@mindrops.com>
2022-11-27 15:21:52 +0530

    Handling Loading & Data States

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     // .then((response) => {
     //   //for converting the json object into javascript object
@@ -22,6 +25,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
 
     // .then((data) => {
     //   const transformedMovies = data.results.map((movieData) => {
@@ -42,7 +46,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
